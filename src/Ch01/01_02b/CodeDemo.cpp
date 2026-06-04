b893e9ba1c5918c27b2b2f238c79bb6128546e86
vkapous-dev <vkapous@gmail.com>
2026-06-04 20:13:15 +0000

    Added code to print message

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 01_02
+// Exercise 01_02 Added code to print message
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hi there!" << std::endl;
+
+    std::cout << std::endl;
+    return 0;
+}
