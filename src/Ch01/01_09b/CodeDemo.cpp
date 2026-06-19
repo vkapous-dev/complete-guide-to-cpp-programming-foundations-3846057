c8b0a40f9c0456d85009054d34e33b1f67fb2193
vkapous-dev <vkapous@gmail.com>
2026-06-19 13:41:29 +0000

    modified code and comments

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -1,11 +1,15 @@
 // Complete Guide to C++ Programming Foundations
-// Challenge 01_09
+// Challenge 01_09 - Modified for lesson
 // Terminal Interaction, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
 
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    std::string name;
+    std::cout << "Enter your name: " << std::flush;
+    std::cin >>name;
+    std::cout << "Nice to meet you, " << name << "!" << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
